c979708eeccbd2bf03250d8e18e26e285224e1af
Brice Rising <brice@algorand.com>
2020-03-26 11:09:40 -0400

    Give failed to load env var warning message some flare
    Now it's yellow and bold

--- mule/util/yaml/env_var_loader.py
+++ mule/util/yaml/env_var_loader.py
@@ -2,13 +2,18 @@ import os
 import re
 import yaml
 from mule.error import messages
+from termcolor import cprint
 
 env_var_path_matcher = re.compile(r'.*\$\{([^}^{]+)\}.*')
 
 def path_constructor(loader, node):
     value = os.path.expandvars(node.value)
     if env_var_path_matcher.match(value):
-        print(messages.CANNOT_EVALUATE_ENV_VAR.format(node.value))
+        cprint(
+            messages.CANNOT_EVALUATE_ENV_VAR.format(node.value),
+            'yellow',
+            attrs=['bold']
+        )
     return os.path.expandvars(node.value)
 
 def getYamlLoaderWithEnvVars():
