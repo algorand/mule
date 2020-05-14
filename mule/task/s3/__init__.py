4c0ee09df2c0ecd969136af4e84081ff7233cd73
Benjamin Toll <benjam72@yahoo.com>
2020-05-14 15:58:23 -0400

    Suggestion to simplify uploading and downloading

--- mule/task/s3/__init__.py
+++ mule/task/s3/__init__.py
@@ -5,26 +5,6 @@ from mule.util import s3_util
 class UploadFile(ITask):
     required_fields = [
         'bucketName',
-        'fileName'
-    ]
-
-    def __init__(self, args):
-        super().__init__(args)
-        self.fileName = args['fileName']
-        self.bucketName = args['bucketName']
-        self.objectName = args['objectName'] if 'objectName' in args else None
-
-    def upload_file(self):
-        s3_util.upload_file(self.fileName, self.bucketName, self.objectName)
-
-    def execute(self, job_context):
-        super().execute(job_context)
-        self.upload_file()
-
-
-class UploadFiles(ITask):
-    required_fields = [
-        'bucketName',
         'globSpec'
     ]
     prefix = None
@@ -47,39 +27,28 @@ class UploadFiles(ITask):
 class DownloadFile(ITask):
     required_fields = [
         'bucketName',
-        'objectName'
+        #        'prefix'
     ]
 
-    def __init__(self, args):
-        super().__init__(args)
-        self.bucketName = args['bucketName']
-        self.objectName = args['objectName']
-        self.outputDir = args['outputDir'] if 'outputDir' in args else '.'
-        self.fileName = args['fileName'] if 'fileName' in args else None
-
-    def download_file(self):
-        s3_util.download_file(self.bucketName, self.objectName, self.outputDir, self.fileName)
-
-    def execute(self, job_context):
-        super().execute(job_context)
-        self.download_file()
-
-
-class DownloadFiles(ITask):
-    required_fields = [
-        'bucketName',
-        'prefix'
-    ]
+#    optional_fields = [
+#        'fileName',
+#        'objectName'
+#    ]
 
     def __init__(self, args):
         super().__init__(args)
         self.bucketName = args['bucketName']
         self.prefix = args['prefix']
         self.suffix = args['suffix'] if 'suffix' in args else None
+        self.fileName = args['fileName'] if 'suffix' in args else None
+        self.objectName = args['objectName'] if 'suffix' in args else None
         self.outputDir = args['outputDir'] if 'outputDir' in args else '.'
 
     def download_files(self):
-        s3_util.download_files(self.bucketName, self.prefix, self.suffix, self.outputDir)
+        if self.objectName or self.filename:
+            s3_util.download_file(self.bucketName, self.objectName, self.outputDir, self.fileName)
+        else:
+            s3_util.download_files(self.bucketName, self.prefix, self.suffix, self.outputDir)
 
     def execute(self, job_context):
         super().execute(job_context)
