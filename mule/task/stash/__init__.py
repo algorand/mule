6e93361a61b674c188a3dbda892fd82a3d00b688
Brice Rising <brice@algorand.com>
2020-05-15 13:46:04 -0400

    Implement unstash
    This will retrieve the stashed file from s3 and unpack it into the current working directory.

--- mule/task/stash/__init__.py
+++ mule/task/stash/__init__.py
@@ -2,6 +2,7 @@ from mule.task import ITask
 from mule.util import s3_util
 from mule.util import time_util
 from mule.util.file_util import compressFiles
+from mule.util.file_util import decompressTarfile
 
 class Stash(ITask):
     required_fields = [
@@ -16,11 +17,31 @@ class Stash(ITask):
         self.bucketName = args['bucketName']
         self.stash_id = args['stash_id']
 
-    def upload_file(self):
+    def stash(self):
         file_name = f"stash-{time_util.get_timestamp()}.tar.gz"
         compressFiles(file_name, self.globSpec)
         s3_util.upload_file(file_name, self.bucketName, f"{self.stash_id}/stash.tar.gz")
 
     def execute(self, job_context):
         super().execute(job_context)
-        self.upload_file()
+        self.stash()
+
+class Unstash(ITask):
+    required_fields = [
+        'bucketName',
+        'stash_id',
+    ]
+
+    def __init__(self, args):
+        super().__init__(args)
+        self.bucketName = args['bucketName']
+        self.stash_id = args['stash_id']
+
+    def unstash(self):
+        file_name = f"stash-{time_util.get_timestamp()}.tar.gz"
+        s3_util.download_file(self.bucketName, f"{self.stash_id}/stash.tar.gz", '.', file_name)
+        decompressTarfile(file_name)
+
+    def execute(self, job_context):
+        super().execute(job_context)
+        self.unstash()
