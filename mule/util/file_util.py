df937ed3e9281e7ef3ad322f8e1804a6b078c8b6
Brice Rising <brice@algorand.com>
2020-05-15 13:13:20 -0400

    Add file util for compressing and decompressing directories
    This compresses files based on globs into tarballs, and the other function decompresses them. This is used for our stash feature.

--- mule/util/file_util.py
+++ mule/util/file_util.py
@@ -1,5 +1,7 @@
 import os
 import yaml
+from glob import glob
+import tarfile
 
 def readYamlFile(path):
     with open(path) as configs:
@@ -15,3 +17,14 @@ def getFilesInFolder(path):
         for filename in filenames:
             files.append(f"{filepath}/{filename}")
     return files
+
+def compressFiles(file_name, globspec):
+    files = glob(globspec, recursive=True)
+    with tarfile.open(file_name, "w:gz") as tar:
+        for file in files:
+            if os.path.isfile(file):
+                tar.add(file)
+
+def decompressTarfile(file_name, target_directory = '.'):
+    with tarfile.open(file_name, "r:gz") as tar:
+        tar.extractall(target_directory)
